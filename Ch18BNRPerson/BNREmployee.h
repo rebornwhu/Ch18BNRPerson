5c632e7035049c2feb795478a22069b71eab1633
xiaolu <rebornwhu@gmail.com>
2015-03-21 13:46:33 -0700

    Clean up

--- Ch18BNRPerson/BNREmployee.h
+++ Ch18BNRPerson/BNREmployee.h
@@ -13,11 +13,12 @@
 {
     NSMutableArray *_assets;
 }
+@property (nonatomic, copy) NSArray *assets;
 
 @property (nonatomic) unsigned int employeeID;
 @property (nonatomic) unsigned int officeAlarmCode;
 @property (nonatomic) NSDate *hireDate;
-@property (nonatomic, copy) NSArray *assets;
+
 
 - (double)yearsOfEmployment;
 - (void)addAsset:(BNRAsset *)a;
--- Ch18BNRPerson/BNREmployee.m
+++ Ch18BNRPerson/BNREmployee.m
@@ -15,7 +15,6 @@
     _assets = [a mutableCopy];
 }
 
-
 - (NSArray *)assets
 {
     return [_assets copy];
